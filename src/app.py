366dee6f4d251e335c4803272a573c7fc56defb2
zpenr <zpenr@bk.ru>
2026-02-13 14:51:50 +0800

    added support for passed values in path

--- src/app.py
+++ src/app.py
@@ -10,13 +10,22 @@ class App():
     
     def __call__(self, environ, start_response):
         path = environ.get('PATH_INFO','/')
-        mask = r'<[A-Za-z]+>'
+        mask = r'(<[A-Za-z]+>)'
         for route in self.routes:
-            new_mask = re.sub(mask,'[^/]+',)
+            new_mask = re.sub(mask,r'(?P\1[^/]+)',route)
             res = re.fullmatch(new_mask,path)
-        else:
-            response_body = '404 NOT FOUND'
-            status = '404 NOT FOUND'
+            if res:
+                parametrs = res.groupdict()
+                handler = self.routes[route]
+                if parametrs:
+                    response_body = handler(parametrs)
+                else:
+                    response_body = handler()
+                status = '200 OK'
+
+        # else:
+        #     response_body = '404 NOT FOUND'
+        #     status = '404 NOT FOUND'
 
         headers = [('Content-Type', 'text/html; charset=utf-8')]
         start_response(status, headers)
