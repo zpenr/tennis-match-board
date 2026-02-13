7b822af5ebe06e7840aaaf219f3f387b60b6882c
zpenr <zpenr@bk.ru>
2026-02-14 02:16:35 +0800

    edited functions to received arguments like **kwargs

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,6 +1,6 @@
 from src.wsgi.app import App
 from ..render_templates import render_template
-from src.db.crud import insert_match, select_player_by_name, insert_player
+from src.db.crud import insert_match, select_player_by_name, insert_player, select_match_by_uuid
 import uuid
 app = App()
 
@@ -10,23 +10,22 @@ def post():
     return render_template('main.html')
 
 @app.route('/match-score')
-def test():
-    return 'da'
+def test(uuid):
+    match = select_match_by_uuid(uuid)[0]
+    return render_template('match-score.html', name1 = match.Player1, name2 = match.Player2, score = match.Score)
 
 @app.route('/new-match')
 def create_match():
     return render_template('create-match.html')
 
 @app.route('/new-match', request_method='POST')
-def create_match(data):
-    player_name1 = data['name1'][0]
-    player_name2 = data['name2'][0]
+def create_match(name1,name2):
 
-    if not(select_player_by_name(player_name1)):insert_player(player_name1)
-    if not(select_player_by_name(player_name2)):insert_player(player_name2)
+    if not(select_player_by_name(name1)):insert_player(name1)
+    if not(select_player_by_name(name2)):insert_player(name2)
 
-    player_id1 = select_player_by_name(player_name1)[0]
-    player_id2 = select_player_by_name(player_name2)[0]
+    player_id1 = select_player_by_name(name1)[0]
+    player_id2 = select_player_by_name(name2)[0]
 
     insert_match(player_id1,player_id2,uuid.uuid4())
 
