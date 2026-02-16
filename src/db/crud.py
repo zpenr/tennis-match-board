b785fc4ff77806e5248009416f40a39114ad982c
zpenr <zpenr@bk.ru>
2026-02-16 23:32:55 +0800

    add BASE_SCORE for score field on table Matches

--- src/db/crud.py
+++ src/db/crud.py
@@ -1,7 +1,24 @@
 from sqlalchemy import text, insert, update, delete,select
 from src.db.database import engine,session_factory, Base
 from src.db.models import PlayersOrm, MatchesOrm
-
+import json 
+BASE_SCORE = json.dumps({
+            'player1':{
+                "score": 0,
+                "win_games": 0,
+                "last_balls": 0,
+                "win_sets": 0,
+                "is_match_win":False
+            },
+            'player2':{
+                "score": 0,
+                "win_games": 0,
+                "last_balls": 0,
+                "win_sets": 0,
+                "is_match_win":False
+            },
+            'match_status': 'in process'
+        })
 def create_tables():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
@@ -15,7 +32,7 @@ def select_player_by_name(name: str):
             .filter_by(Name = name)
             )
         ans = session.execute(query)
-        print(ans.first())
+        return ans.first()
 
 def insert_player(name: str):
     player = PlayersOrm(Name = name)
@@ -23,12 +40,15 @@ def insert_player(name: str):
         session.add(player)
         session.commit()
 
-def insert_match(player_id1: int,  player_id2: int, UUID: str, score: int, winner: str = None):
+def insert_match(player_id1: int,  player_id2: int, UUID: str, score: str = BASE_SCORE, winner: str = None):
     match = MatchesOrm( UUID = UUID, Player1 = player_id1, Player2 = player_id2, Winner = winner, Score = score)
     with session_factory() as session:
         session.add(match)
         session.commit()
         print(session.execute(select(MatchesOrm.ID)).first())
 
-# create_tables()
-# insert_match(1,2,'qwtery',"12")
+def select_match_by_uuid(uuid: str):
+    with session_factory() as session:
+        query = select(MatchesOrm).where(MatchesOrm.UUID == uuid)
+        match = session.execute(query).scalar()
+        return match
